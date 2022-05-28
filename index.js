dd52206ffb7c8b97fef980620c7bee729da4f1b4
UmmeHania <87921374+UmmeHania@users.noreply.github.com>
2022-05-28 23:57:04 +0600

    delete

--- index.js
+++ index.js
@@ -132,6 +132,14 @@ async function run() {
             res.send(result);
         })
 
+        //DELETE 
+        app.delete("/order/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        });
+
         //load orders placed by specific user
         app.get('/order', verifyJWT, async (req, res) => {
             const customer = req.query.customer;
